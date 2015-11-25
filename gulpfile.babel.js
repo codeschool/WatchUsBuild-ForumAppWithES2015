0ba4981669da2d6ff9afc98f46a26255f182039b
Carlos Souza <carloshrsouza@gmail.com>
2015-11-25 11:30:28 -0200

    Add gulp watch task

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -2,7 +2,9 @@ import gulp from "gulp";
 import browserify from "browserify";
 import source from "vinyl-source-stream";
 
-gulp.task("default", () => {
+gulp.task("default", ["watch"]);
+
+gulp.task("transpile", () => {
 
   return browserify("src/app.js")
     .transform("babelify")
@@ -10,5 +12,11 @@ gulp.task("default", () => {
     .pipe(source("bundle.js"))
     .pipe(gulp.dest("dist"));
 
+});
+
+
+
+gulp.task("watch", ["transpile"], () => {
+  gulp.watch("src/**/*", ["transpile"]);
 });
 
